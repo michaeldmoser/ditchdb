2169bf16f9d8f70a6136819f9f545a9f55cd09f3
Michael Moser <michaeldmoser@gmail.com>
2023-08-12 20:24:40 -0600

    Reorganize

---
+++ frontend/src/features/properties/components/search.tsx
@@ -0,0 +1,36 @@
+import { useGetPropertiesQuery } from "../api";
+import { Link } from "react-router-dom";
+
+export default function PropertiesSearch() {
+  return <List />;
+}
+
+function ListItem({ property }: { property: Property }) {
+  return (
+    <li className="flex justify-between gap-x-2 border-b border-base-content border-solid">
+      <Link to={property.id.toString()} className="link link-hover p-2 grow">
+        {property.addr_number} {property.addr_predirectional}{" "}
+        {property.addr_street} {property.addr_roadsuffix}{" "}
+        {property.addr_postdirectional}
+      </Link>
+    </li>
+  );
+}
+
+function List() {
+  const { data, isLoading } = useGetPropertiesQuery();
+
+  return isLoading ? <div>Loading...</div> : (
+    <section>
+      <h2 id="properties-title" className="text-2xl mb-2">Properties</h2>
+      <ul
+        aria-labelledby="properties-title"
+        className="bg-neutral text-neutral-content"
+      >
+        {data?.results?.map((property: Property, key) => (
+          <ListItem key={key} property={property} />
+        ))}
+      </ul>
+    </section>
+  );
+}
--- frontend/src/features/properties/layout.tsx
+++ frontend/src/features/properties/layout.tsx
@@ -1,22 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-import List from './list';
-import PropertyDetail from './components/detail';
-import PropertyMap from './components/map';
+import { Route, Routes } from "react-router-dom";
+import PropertyDetail from "./components/detail";
+import PropertyMap from "./components/map";
+import PropertiesSearch from "./components/search";
 
 /**
  * Layout for properties screen
- *
- * @function Layout
- * @returns {JSX.Element} properties page layout
  */
 export default function Layout() {
   return (
     <article>
-      <h2 className="text-2xl">Properties</h2>
-      <div className="grid grid-cols-[2fr_10fr] gap-4">
-        <section className='bg-neutral text-neutral-content'>
-          <List />
-        </section>
+      <div className="grid grid-cols-[2fr_10fr] gap-4 p-2">
+        <PropertiesSearch />
         <section>
           <Routes>
             <Route path="/:id" element={<PropertyDetail />} />
