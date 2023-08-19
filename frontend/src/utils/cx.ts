f1d3e870dd22495e21a1ff86e24ed7fadd6813fb
Michael Moser <michaeldmoser@gmail.com>
2023-08-19 07:48:27 -0600

    Create css class utility function

---
+++ frontend/src/utils/cx.ts
@@ -0,0 +1,10 @@
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Easily combine CSS classes conditionally on an element and merge Tailwind classes. This function is a wrapper around `clsx` and `tailwind-merge` to make it easier to
+ */
+export default function cx(...args: any[]) {
+  const classes = clsx(...args);
+  return twMerge(classes);
+}
