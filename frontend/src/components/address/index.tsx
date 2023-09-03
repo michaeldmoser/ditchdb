81754c31b9b48fc33f230f917eb1743f18f8bb08
Michael Moser <michaeldmoser@gmail.com>
2023-09-02 20:23:21 -0600

    Fix types

--- frontend/src/components/address/index.tsx
+++ frontend/src/components/address/index.tsx
@@ -2,11 +2,11 @@ import { HTMLAttributes } from "react";
 
 type AddressProps = HTMLAttributes<HTMLDivElement> & {
   address1: string | null | undefined;
-  address2?: string;
-  address3?: string;
+  address2?: string | null | undefined;
+  address3?: string | null | undefined;
   city: string;
   state: string;
-  zip: string;
+  zip?: string | null | undefined;
 };
 
 /**
