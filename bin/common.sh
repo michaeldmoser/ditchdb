5432e7437e4964872c8c89f30693a2f314f49f08
Michael Moser <michaeldmoser@gmail.com>
2023-09-13 06:14:42 -0600

    Clean up use of docker compose

---
+++ bin/common.sh
@@ -0,0 +1,12 @@
+PROJECT_ROOT_DIR=`dirname ${0:a:h}`
+export PATH="${PROJECT_ROOT_DIR}/bin:$PATH"
+
+docker_exec_dev() {
+    docker compose exec -it dev zsh -c "$@"
+}
+
+ensure_in_project_root() {
+    if [ `pwd` = "$PROJECT_ROOT_DIR" ]; then return 0; fi
+
+    pushd $PROJECT_ROOT_DIR
+}
